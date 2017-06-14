a408a4a8c243abe4b22e3675cf3fecf3185862c1
Ramneek Handa <RamneekHanda@gmail.com>
2017-06-15 01:26:05 +0800

    minor changes

--- android/src/main/java/com/rnffmpeg/FFMpeg.java
+++ android/src/main/java/com/rnffmpeg/FFMpeg.java
@@ -1,5 +1,8 @@
 package com.rnffmpeg;
 
 public class FFMpeg {
+    static {
+        System.loadLibrary("ffmpeg_wrap");
+    }
     public native static int encodeVideo(String filename, String outputfile);
 }
